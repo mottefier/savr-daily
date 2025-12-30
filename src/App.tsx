05486fcd8d548953e15f46126a5a2b44b24d2554
phalink <browning89@gmail.com>
2025-12-29 20:54:47 -0800

    questions/input states added

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
 import { useState } from 'react'
+import { useEffect} from 'react'
 import './App.css'
 
 function App() {
+  type PageState = 'question-1' | 'question-2' | 'question-3';
 
-  const [state, setState] = useState({
-    incomeQuestion: 'what is your monthly income?',
-    response: 0 
-  });
+  const [currentState, setCurrentState] = useState<PageState>('question-1');
 
-  function updateResponse(event:any){
-    setState({
-      ...state,
-      response: event.target.value
-    });
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Enter' && currentState === 'question-1') {
+        setCurrentState('question-2')
+      }
+      else if (event.key === 'Enter' && currentState === 'question-2') {
+        setCurrentState('question-3');
+    }
   };
 
-  function inputKeyPress(event:any){
-   if(event.key=== "Enter") {
-    let answer = state.response
-    return answer
-  };
+  window.addEventListener('keydown', handleKeyDown);
+
+  return () => window.removeEventListener('keydown', handleKeyDown);
+}, [currentState]);
+
 
   return (
     <>
      <div>
-      <div>{state.incomeQuestion}</div>
-      <input onKeyUp={inputKeyPress} onChange={updateResponse} value={state.response}></input>
+      {currentState === 'question-1' && (
+        <div className="page">
+          <h2>What is your monthly income?</h2>
+          <input></input>
+          </div>
+      )}
+
+      {currentState === 'question-2' && (
+        <div className="page">
+          <h2>What is your monthly mortgage/rent payment?</h2>
+          <input></input>
+          </div>
+      )}
+
+      {currentState === 'question-3' && (
+        <div className="page">
+          <h2>How much did you spend today?</h2>
+          <input></input>
+          </div>
+      )}
       </div>
       </>
   );
-}
 }
 export default App
